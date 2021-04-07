5b088fef7193f85b5877682c4e25576907f156bd
bishnunathbd <bishnunath774@gmail.com>
2021-04-07 08:47:45 +0600

    34-13-added cart component calculation

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
@@ -23,8 +24,7 @@ const Shop = () => {
         }
       </div>
       <div className="cart-container">
-        <h3>This is Cart.</h3>
-        <h5>Order Summary: {cart.length}</h5>
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
