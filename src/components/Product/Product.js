c6b46ba52320f54b399444df8b6a3d0118bb3897
bishnunathbd <bishnunath774@gmail.com>
2021-04-08 12:04:17 +0600

    37-4-dynamic path, set route path parameter

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
+import { Link } from 'react-router-dom';
 
 const Product = (props) => {
-  const {name, img, price, seller, stock} = props.product;
+  const {name, img, price, seller, stock, key} = props.product;
   const handleAddToCart = props.handleAddToCart;
 
   return (
@@ -13,7 +14,7 @@ const Product = (props) => {
         <img src={img} alt=""/>
       </div>
       <div>
-        <h4 className='product-name'>{name}</h4>
+        <h4 className='product-name'><Link to={'/product/'+key}>{name}</Link></h4>
         <br/>
         <p><small>by: {seller}</small></p>
         <p>${price}</p>
