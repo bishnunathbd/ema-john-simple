165cd0dcd901bb0f2e46528c000303da2473d1ea
bishnunathbd <bishnunath774@gmail.com>
2021-04-08 16:07:43 +0600

    37-7-save cart state on localstore

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import { addToDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -11,16 +12,18 @@ const Shop = () => {
   const [cart, setCart] = useState([]);
 
   const handleAddToCart = (product) => {
-    console.log('product added', product);
     const newCart = [...cart, product];
     setCart(newCart);
+    const sameProduct = newCart.filter(pd => pd.key === product.key);
+    const count = sameProduct.length;
+    addToDatabaseCart(product.key, count);
   }
 
   return (
     <div className='shop-container'>
       <div className="product-container">
         {
-          products.map(pd => <Product product={pd} handleAddToCart={handleAddToCart} showAddToCart={true}></Product>)
+          products.map(pd => <Product product={pd} handleAddToCart={handleAddToCart} showAddToCart={true} key={pd.key}></Product>)
         }
       </div>
       <div className="cart-container">
