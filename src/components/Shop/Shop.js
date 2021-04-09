b2a9627d8c4acea240f12cba37e274f65c4bd37d
bishnunathbd <bishnunath774@gmail.com>
2021-04-09 11:55:06 +0600

    38-5-sync state in multiple route

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { addToDatabaseCart } from '../../utilities/databaseManager';
+import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -11,10 +11,21 @@ const Shop = () => {
   const [products, setProducts] = useState(first10);
   const [cart, setCart] = useState([]);
 
+  useEffect(() => {
+    const savedCart = getDatabaseCart();
+    const productKeys = Object.keys(savedCart);
+    const previousCart = productKeys.map(key => {
+      const product = fakeData.find(product => product.key === key);
+      product.quantity = savedCart[key];
+      return product;
+    })
+    setCart(previousCart);
+  }, [])
+
   const handleAddToCart = (product) => {
     const toBeAddedKey = product.key;
     const sameProduct = cart.find(pd => pd.key === toBeAddedKey);
-    let count =1;
+    let count = 1;
     let newCart;
     if (sameProduct) {
       count = sameProduct.quantity + 1;
@@ -22,7 +33,7 @@ const Shop = () => {
       const others = cart.filter(pd => pd.key !== toBeAddedKey);
       newCart = [...others, sameProduct];
     }
-    else{
+    else {
       product.quantity = count;
       newCart = [...cart, product];
     }
